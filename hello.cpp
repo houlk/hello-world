23730a4f67092ecbc1589d57488fd91976713f31
houlk <houlk@sjtu.edu.cn>
2018-05-18 17:15:43 +0800

    Another modification on the hello file

--- hello.cpp
+++ hello.cpp
@@ -5,5 +5,8 @@ int main(void)
 {
     cout << "Hello earth!" << endl;
     cout << "I am an alien frog! Gua gua!" << endl;
+    int m = 5;
+    int n = 10;
+    cout << "m times n gives the result " << m*n << endl;
     return 1;
 }
