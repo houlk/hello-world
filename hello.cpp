ffd247796d72cf62f3b059a9aae9277aa1fd9f48
Likun Hou <houlk@sjtu.edu.cn>
2018-05-18 17:30:44 +0800

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -8,5 +8,6 @@ int main(void)
     int m = 5;
     int n = 10;
     cout << "m times n gives the result " << m*n << endl;
+    cout << "Maybe I should stay here for a while" << endl;
     return 1;
 }
