f186c9b805dca8eb536ecb667b21e564b51e1f8a
houlk <hmooncake@gmail.com>
2018-05-19 17:09:35 +0800

    Add 3^2

--- vectest.cpp
+++ vectest.cpp
@@ -12,6 +12,7 @@ int main(void)
     {
         vect[i] = make_pair(i,pow(i,2));
     }
-    cout << "Square of 5 is " << vect[5].second << endl;
+    cout << "The square of 5 is " << vect[5].second << endl;
+    cout << "The square of 3 is " << vect[3].second << endl;
     return 1;
 }
